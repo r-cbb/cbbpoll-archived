6626b1de461a762dd3483bcdc87368f00e605f53
Daniel Harms <jdharms@gmail.com>
2019-08-02 11:17:57 -0400

    Add code to handle auto-incrementing ID fields.
    Some tricky transaction/concurrency stuff required
    to implement a unique auto-incrementing ID field
    in Google Cloud Datastore, because the database
    does not support unique constraints.  We solve the
    problem by performing an atomic "Get or Add", and
    throw an error if the Get succeeds.  We should handle
    this error and retry--not yet sure whether to do that
    in the persistence layer (probably) or application logic.
    
    Signed-off-by: Daniel Harms <jdharms@gmail.com>

--- backend/internal/db/datastore.go
+++ backend/internal/db/datastore.go
@@ -12,6 +12,31 @@ type DBClient struct {
 	client *datastore.Client
 }
 
+type IDStruct struct {
+	ID int64
+}
+
+func (i *IDStruct) Load(property []datastore.Property) error {
+	var ok, foundId bool
+	for _, v := range property {
+		if v.Name == "ID" {
+			i.ID, ok = v.Value.(int64)
+			if !ok {
+				return fmt.Errorf("error loading ID property")
+			}
+			foundId = true
+		}
+	}
+	if !foundId {
+		return fmt.Errorf("no ID property on load")
+	}
+	return nil
+}
+
+func (i IDStruct) Save() ([]datastore.Property, error) {
+	return nil, fmt.Errorf("Should never save an IDStruct to storage")
+}
+
 func NewDBClient(projectId string) (*DBClient, error) {
 	ctx := context.Background()
 
@@ -32,44 +57,77 @@ func NewDBClient(projectId string) (*DBClient, error) {
 	return &DBClient{client: client}, nil
 }
 
+func (db *DBClient) nextID(kind string) (id int64, err error) {
+	ctx := context.Background()
+	q := datastore.NewQuery(kind).Order("-ID")
+	var ids []IDStruct
+
+	_, err = db.client.GetAll(ctx, q, &ids)
+	if err != nil {
+		return
+	}
+
+	if len(ids) == 0 {
+		id = 1
+	} else {
+		id = ids[0].ID + 1
+	}
+
+	return
+}
+
 func (db *DBClient) AddTeam(team cbbpoll.Team) (id int64, err error) {
 	ctx := context.Background()
-	k := datastore.IncompleteKey("Team", nil)
 
-	q := datastore.NewQuery("Team").Order("-ID").Limit(1)
-
-	var teams []cbbpoll.Team
-
-	keys, err := db.client.GetAll(ctx, q, &teams)
-
-	var newId int64
-	newId = 0
-	if len(keys) > 0 {
-		fmt.Printf("greatest ID found: %v", teams[0].ID)
-		newId = teams[0].ID + 1
-	}
-
-	team.ID = newId
-
-	k, err = db.client.Put(ctx, k, &team)
+	newId, err := db.nextID("Team")
 	if err != nil {
-		return 0, fmt.Errorf("datastoredb: could not put Team: %v", err)
+		fmt.Printf("error finding next ID: %v", err.Error())
+		return 0, err
 	}
+	team.ID = newId
+	k := datastore.IDKey("Team", newId, nil)
+
+	tx, err := db.client.NewTransaction(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("datastoredb: could not create transaction: %v", err)
+	}
+
+	var tmp cbbpoll.Team
+
+	// Perform a Get or Put to ensure atomicity
+	err = tx.Get(k, &tmp)
+	if err == nil || err != datastore.ErrNoSuchEntity{
+		_ = tx.Rollback()
+		return 0, fmt.Errorf("concurrency error adding Team")
+	}
+
+	pk, err := tx.Put(k, &team)
+	if err != nil {
+		_ = tx.Rollback()
+		return 0, fmt.Errorf("datastoredb: could not put team entity: %v", err)
+	}
+
+	c, err := tx.Commit()
+	if err != nil {
+		return 0, fmt.Errorf("datastoredb: error committing transaction: %v", err)
+	}
+
+	k = c.Key(pk)
+	if k.ID != newId {
+		panic("keys don't match")
+	}
+
 	return newId, nil
 }
 
 func (db *DBClient) GetTeam(id int64) (team cbbpoll.Team, err error) {
 	ctx := context.Background()
 
-	q := datastore.NewQuery("Team").Filter("ID =", id)
-
-	var teams []cbbpoll.Team
-
-	keys, err := db.client.GetAll(ctx, q, &teams)
-
-	if len(keys) != 1 {
-		return cbbpoll.Team{}, fmt.Errorf("not found")
+	k := datastore.IDKey("Team", id, nil)
+	err = db.client.Get(ctx, k, &team)
+	if err != nil {
+		return cbbpoll.Team{}, fmt.Errorf("error getting team: %v", err)
 	}
 
-	return teams[0], nil
+	return
 }
